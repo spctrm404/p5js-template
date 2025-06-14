a4fa105b8db2af9e5f8535b26e853c952d57da26
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2025-06-14 15:37:35 +0900

    feat: Add p5template files including styles, responsive design, and reference management
    - Introduced p5template.js and p5template.scss for core template functionality and styling.
    - Implemented references.js to manage and display reference lists in APA format.
    - Created responsive.js to handle responsive canvas resizing based on container dimensions.
    - Developed scroll.js for smooth scrolling behavior between sections.
    - Added title.js to dynamically set the document title and author information.

---
+++ common/p5template/title.js
@@ -0,0 +1,51 @@
+class Title {
+  title;
+  designer;
+  year;
+  selectorTitle;
+  selectorAuthor;
+
+  /**
+   * 제목, 디자이너, 연도를 받아 템플릿에 반영.
+   *
+   * @param {string} [title='키스의 고유 조건은 입술끼리 만나야 하고 특별한 기술은 필요치 않다'] - 작품 제목
+   * @param {string} [designer='손우성'] - 디자이너 이름
+   * @param {string} [year='2025'] - 연도
+   * @param {Object} [options={ selectorTitle: '.title', selectorAuthor: '.author' }] - 옵션 객체 (선택):
+   *   - selectorTitle: 제목 템플릿 요소 선택자 (기본값: '.title')
+   *   - selectorAuthor: 저자 템플릿 요소 선택자 (기본값: '.author')
+   */
+  constructor(
+    title = '키스의 고유 조건은 입술끼리 만나야 하고 특별한 기술은 필요치 않다',
+    designer = '손우성',
+    year = '2025',
+    options = { selectorTitle: '.title', selectorAuthor: '.author' }
+  ) {
+    this.title = title;
+    this.designer = designer;
+    this.year = year;
+    this.selectorTitle = options.selectorTitle || '.title';
+    this.selectorAuthor = options.selectorAuthor || '.author';
+
+    this.init();
+  }
+
+  #setHtmlTitle() {
+    document.title = `${this.designer} - ${this.title}`;
+  }
+
+  #setHtmlBody() {
+    document
+      .querySelectorAll(this.selectorTitle)
+      .forEach((el) => (el.textContent = this.title));
+
+    document
+      .querySelectorAll(this.selectorAuthor)
+      .forEach((el) => (el.textContent = `${this.designer} (${this.year})`));
+  }
+
+  init() {
+    this.#setHtmlTitle();
+    this.#setHtmlBody();
+  }
+}
